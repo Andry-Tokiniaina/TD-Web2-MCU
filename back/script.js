ba4dc89e9c0332a68d02f9b89b27fa918b5e153e
Toky <tokiniainarakotozanany@gmail.com>
2025-08-15 17:33:57 +0300

    travail sur script.js qui gère les verbes http

--- back/script.js
+++ back/script.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import express, { json } from 'express';
 
 const app = express()
+
 app.use(json())
 
 const __filename = fileURLToPath(import.meta.url);
@@ -11,33 +12,44 @@ const __dirname = path.dirname(__filename);
 
 const filePath = path.join(__dirname, 'characters.json')
 
-let charactersList;
-
-fs.readFile(filePath, 'utf8', (err, data) =>{
-    if (err) {
-        console.log(err);
-        
-        return [];
-    }
-    try {
-        charactersList = JSON.parse(data).characters
-        console.log(charactersList);
-        return JSON.parse(data);
-
-    }
-    catch (err) {
-        return [];
-    }
-})
+let charactersList = JSON.parse(fs.readFileSync(filePath, 'utf8')).characters
 
 app.get("/characters", (req, res)=>{
     res.send(charactersList)
 })
 
-app.post("characters", (req, res) => {
-    const chars = req.body.characters;
+app.post("/characters", (req, res) => {    
+    const chars = req.body?.characters;
+    if (!chars) {
+        res.status(400).send("erreur")
+    }
     charactersList = charactersList.concat(chars)
-    res.send("Characters added")
+    res.send(charactersList)
+})
+
+app.put("/characters", (req, res)=>{
+    const charReq = req.body?.characters;
+    if (charReq) {
+        let newCharList = []
+        for(let char of charReq){
+            let find = false
+            for(let char2 of charactersList){
+                if(char.id == char2.id){
+                    find = true
+                    char2 = char
+                }
+                newCharList.push(char2)
+            }
+            if (!find) {
+                newCharList.push(char)
+            }
+        }
+        charactersList = newCharList
+    }
+    else{
+        res.status(400)
+    }
+    res.send(charactersList)
 })
 
 app.listen(8080)
